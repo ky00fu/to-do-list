26515ea7f8f97b047fd8a9923cac1e2f7b36ec47
ky00fu <kirsycu@gmail.com>
2023-11-28 16:34:14 -0300

    imported backend

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+
+const router = require('./src/routes/todo')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/', router)
+
+app.listen(3000, () => {
+    console.log("Funcionando")
+})
