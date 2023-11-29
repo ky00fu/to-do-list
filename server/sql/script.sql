90e4b02b7e78daa3d108080d00506f896bfe171b
ky00fu <kirsycu@gmail.com>
2023-11-28 21:40:15 -0300

    updated sql

--- server/sql/script.sql
+++ server/sql/script.sql
@@ -3,15 +3,16 @@ CREATE DATABASE todolist CHARSET=UTF8 COLLATE utf8_general_ci;
 USE todolist;
 
 CREATE TABLE usuario (
-    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(50),
-    telefone VARCHAR(20),
-    email VARCHAR(30),
-    senha VARCHAR(20)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    senha TEXT NOT NULL
 );
 
 CREATE TABLE tarefa (
     id INT PRIMARY KEY AUTO_INCREMENT,
+    usuarioid INT NOT NULL,
     description TEXT NOT NULL,
-    status VARCHAR(10) NOT NULL
+    status INT NOT NULL,
+    FOREIGN KEY (usuarioid) REFERENCES usuario(id)
 );
---
+++ server/sql/testes.sql
@@ -0,0 +1,8 @@
+INSERT INTO usuario VALUES
+(NULL,'João Silva', 'joao.silva@gmail.com', PASSWORD('senha123')),
+(NULL,'Maria Santos', 'maria.santos@hotmail.com', PASSWORD('senha456')),
+(NULL,'Pedro Costa', 'pedro.costa@yahoo.com.br', PASSWORD('senha789'));
+
+INSERT INTO tarefa VALUES
+(NULL, 1, 'Terminar projeto final', 0),
+(NULL, 3,'Ir ao supermercado', 1);
